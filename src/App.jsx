60d37e98b8011a400108821b191a544358a833f0
shahadot99999 <shahadot99999@gmail.com>
2025-05-14 11:16:31 +0600

    (advanced) Reusable form using props and children

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,47 @@
 import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import ReuseableForm from './components/ReuseableForm/ReuseableForm'
+//import HookForm from './components/HookForm/HookForm'
 //import RefFrom from './components/RefFrom/RefFrom'
 //import StateFulForm from './components/StateFulForm/StateFulForm'
 //import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
 
+   const handleSinUpSubmit = data =>{
+        //e.preventDefault();
+        console.log('sign up dta', data);
+    }
+
+    const handleUpDateProfile = data =>{
+      console.log('update profile', data);
+    }
 
   return (
     <>
       
-      <h1>Vite + React</h1>
+      {/* <h1>Vite + React</h1> */}
       {/* <SimpleForm></SimpleForm> */}
        {/* <StateFulForm></StateFulForm>     */}
        {/* <RefFrom></RefFrom> */}
-       <HookForm></HookForm>
+       {/* <HookForm></HookForm> */}
+       <ReuseableForm 
+       formTitle={'Sign up'}
+        handleSubmit={handleSinUpSubmit}>
+         <div>
+          <h2>Sign Up</h2>
+          <p>please sign up right now</p>
+         </div>
+
+        </ReuseableForm>
+       <ReuseableForm 
+       formTitle={'Profile update'} 
+       handleSubmit={handleUpDateProfile}
+        submitBtnText={'Update'}>
+          <div>
+            <h2>Update profile</h2>
+            <p>Always keep your profile updated</p>
+          </div>
+        </ReuseableForm>
     </>
   )
 }
